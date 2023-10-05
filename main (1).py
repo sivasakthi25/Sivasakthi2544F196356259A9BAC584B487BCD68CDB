32773b3ce7be213f6a8d191af48420cb6fb96315
sivasakthi25 <144420658+sivasakthi25@users.noreply.github.com>
2023-10-05 12:04:32 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,24 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-# Example usage:
-number = int(input("enter the number:"))
-result = factorial(number)
-print(f"The factorial of {number} is {result}")
+def sort_students(student_list):
+    # Sort the student list in descending order of CGPA
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+
+# Test the function with a list of student objects
+students = [
+    Student("sridhar", "A101", 3.9),
+    Student("velan", "B102", 3.7),
+    Student("ragav", "C103", 3.8),
+    Student("seenu", "D104", 3.5),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-def factorial(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
-def factorial(n):
-  if n == 0:
-    return 1 
-  else:
-    return n * factorial(n - 1)
-print (factorial(5)) 
+def linear_search_product(products, target_product):
+    indices = []
+    for i, product in enumerate(products):
+        if product == target_product:
+            indices.append(i)
+    return indices
+
+# Example usage:
+products = ["apple", "banana", "apple", "orange", "grape", "apple"]
+target_product = "apple"
+result = linear_search_product(products, target_product)
+print(result)  # Output: [0, 2, 5]
